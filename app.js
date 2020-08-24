80c1ca6ed917eaba4557d1be00fa4938e89f7e8a
Daniel A <dannyboy3385@gmail.com>
2020-08-24 13:18:13 -0700

    View employees complete

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql2')
 
+const db = mysql.createConnection('mysql://root:-Fisher3385-@localhost/employee_db')
+
 const addEmployee = () => {
   console.log('Adding Employee')
 }
 
 const viewEmployees = () => {
   console.log('Viewing Employees')
+  db.query(`
+  SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN role 
+  ON employee.role_id = role.id
+  LEFT JOIN department
+  ON role.department_id = department.id
+  LEFT JOIN employee manager
+  ON manager.id = employee.manager_id
+  `, (err, data) => {
+    if (err) {console.log(err)}
+    console.log(data)
+  })
 }
 
 const updateRole = () => {
