0f78d147584b59cab4a862e6a8aa7afb8f60b850
Daniel A <dannyboy3385@gmail.com>
2020-08-25 18:17:50 -0700

    README created

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # employeeManager
+
+> This application is a team card creator that allows you to easily and effectively create cards for employees to have all of thier necessary information on hand!
+
+![License Badge](https://img.shields.io/badge/license-none-blue.svg)
+
+### Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [Questions](#questions)
+
+
+## Installation:
+ To install the necessary npm files for this application, open the terminal and type "npm i" to import all necessary npm files.
+
+ ![npm example](https://i.ibb.co/0Xyn3gZ/team-Creator-npmi.png)
+
+
+## Usage:
+ To start the application, once again open the terminal and type "node app.js". This will begin the application and begin to give prompts to add view, and edit employees. 
+
+ ![usage example](https://i.ibb.co/b3GbnT1/Employee-Manager.png)
+
+Depending on the action selected, you will either see a formatted version of the data in the terminal: 
+
+ ![usage example](https://i.ibb.co/c1nBfB4/Veiwing-Employees.png)
+
+ Or will be prompted about information to add to the database:
+
+ ![usage example](https://i.ibb.co/rw6ygrS/adding-Department.png)
+
+###Youtube link to usage video:
+https://youtu.be/Q5kFfJ0EYaA
+
+### Questions:
+**What is your GitHub username?** My GitHub account can be found at https://github.com/DaniBoie
+**Email to reach for additional questions:** dannyboy3385@gmail.com
--- app.js
+++ app.js
@@ -6,57 +6,57 @@ const db = mysql.createConnection('mysql://root:-Fisher3385-@localhost/employee_
 const addEmployee = () => {
   console.log('Adding Employee')
   db.query('SELECT * FROM role', (err, roles) => {
-    if (err){console.log(err)}
-    
+    if (err) { console.log(err) }
+
     roles = roles.map(role => ({
       name: role.title,
       value: role.id
     }))
-    
+
     db.query('SELECT * FROM employee', (err, employees) => {
-      if (err){console.log(err)}
-      
+      if (err) { console.log(err) }
+
       employees = employees.map(employee => ({
         name: `${employee.first_name} ${employee.last_name}`,
         value: employee.id
       }))
 
-      employees.unshift({name: 'None', value: null})
-      
+      employees.unshift({ name: 'None', value: null })
+
       inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'first_name',
-          message: "What is the employee's first name"
-        },
-        {
-          type: 'input',
-          name: 'last_name',
-          message: "What is the employee's last name"
-        },
-        {
-          type: 'list',
-          name: 'role_id',
-          message: 'Choose a role for the employee',
-          choices: roles
-        },
-        {
-          type: 'list',
-          name: 'manager_id',
-          message: 'Choose a manager for the employee:',
-          choices: employees
-        }
-      ])
-      .then(employee => {
-        console.log(employee)
-        db.query('INSERT INTO employee SET ?', employee, (err) => {
-          if (err){console.log(err)}
-          console.log('Employee Created!')
-          landingPrompt()
+        .prompt([
+          {
+            type: 'input',
+            name: 'first_name',
+            message: "What is the employee's first name"
+          },
+          {
+            type: 'input',
+            name: 'last_name',
+            message: "What is the employee's last name"
+          },
+          {
+            type: 'list',
+            name: 'role_id',
+            message: 'Choose a role for the employee',
+            choices: roles 
+          },
+          {
+            type: 'list',
+            name: 'manager_id',
+            message: 'Choose a manager for the employee:',
+            choices: employees
+          }
+        ])
+        .then(employee => {
+          console.log(employee)
+          db.query('INSERT INTO employee SET ?', employee, (err) => {
+            if (err) { console.log(err) }
+            console.log('Employee Created!')
+            landingPrompt()
+          })
         })
-      })
-      .catch(err => console.log(err))
+        .catch(err => console.log(err))
     })
   })
 }
@@ -73,16 +73,16 @@ const viewEmployees = () => {
   LEFT JOIN employee manager
   ON manager.id = employee.manager_id
   `, (err, employees) => {
-    if (err) {console.log(err)}
+    if (err) { console.log(err) }
     console.table(employees)
-      landingPrompt()
+    landingPrompt()
   })
 }
 
 const updateRole = () => {
   console.log('Updating Employees')
   db.query('SELECT * FROM employee', (err, employees) => {
-    if (err){console.log(err)}
+    if (err) { console.log(err) }
 
     employees = employees.map(employee => ({
       name: `${employee.first_name} ${employee.last_name}`,
@@ -96,52 +96,52 @@ const updateRole = () => {
         name: role.title,
         value: role.id
       }))
-    
 
-    inquirer
-      .prompt([
-        {
-          type: 'list',
-          name: 'employee',
-          message: 'Which employee would you like to change?',
-          choices: employees
-        },
-        {
-          type: 'list',
-          name: 'role',
-          message: 'What new role should be given?',
-          choices: roles
-          
-        }
-      ])
-      .then(res => {
-        db.query('UPDATE employee SET role_id = ? WHERE id = ?', [res.role, res.employee])
-        console.log('Updated Employee Role!')
-        landingPrompt()
-      })
-      .catch(err => {console.log(err)})
+
+      inquirer
+        .prompt([
+          {
+            type: 'list',
+            name: 'employee',
+            message: 'Which employee would you like to change?',
+            choices: employees
+          },
+          {
+            type: 'list',
+            name: 'role',
+            message: 'What new role should be given?',
+            choices: roles
+
+          }
+        ])
+        .then(res => {
+          db.query('UPDATE employee SET role_id = ? WHERE id = ?', [res.role, res.employee])
+          console.log('Updated Employee Role!')
+          landingPrompt()
+        })
+        .catch(err => { console.log(err) })
+    })
   })
-})
 }
 
 const addDepartment = () => {
   console.log('Adding Department')
   inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Name of new department:'
-    }
-  ])
-  .then(department => {
-    db.query('INSERT INTO department SET ?', department, (err) => {
-      if (err) { console.log(err) }
-      console.log('Department Created!')
-      landingPrompt()
-  })
-  })
-  .catch(err => {console.log(err)})
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name of new department:'
+      }
+    ])
+    .then(department => {
+      db.query('INSERT INTO department SET ?', department, (err) => {
+        if (err) { console.log(err) }
+        console.log('Department Created!')
+        landingPrompt()
+      })
+    })
+    .catch(err => { console.log(err) })
 }
 
 const viewDepartments = () => {
@@ -150,7 +150,7 @@ const viewDepartments = () => {
   SELECT department.id, department.name AS Departments
   FROM department
   `, (err, res) => {
-    if (err) {console.log(err)}
+    if (err) { console.log(err) }
     console.table(res)
     landingPrompt()
   })
@@ -190,101 +190,101 @@ const addRole = () => {
 const viewRoles = () => {
   console.log('Viewing Roles')
   db.query(`
-  SELECT role.title AS Title, role.salary AS Salary, department.name AS Department
+  SELECT role.id, role.title AS Title, role.salary AS Salary, department.name AS Department
   FROM role
   LEFT JOIN department
   ON role.department_id = department.id
   `, (err, res) => {
-    if (err){console.log(err)}
+    if (err) { console.log(err) }
     console.table(res)
     landingPrompt()
   })
 }
 function landingPrompt() {
   inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'redo',
-      message: 'Start another action?',
-      choices: ['Yes', 'No']
-    }
-  ])
-  .then(res => {
-    if (res.redo === 'Yes'){
-      start()
-    }else {
-      process.exit()
-    }
-  })
-  .catch(err => {console.log(err)})
+    .prompt([
+      {
+        type: 'list',
+        name: 'redo',
+        message: 'Start another action?',
+        choices: ['Yes', 'No']
+      }
+    ])
+    .then(res => {
+      if (res.redo === 'Yes') {
+        start()
+      } else {
+        process.exit()
+      }
+    })
+    .catch(err => { console.log(err) })
 }
 
 function start() {
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'option',
-      message: 'What action would you like to perform?',
-      choices: [
-        {
-          name: 'Add An Employee',
-          value: 'addEmployee'
-        },
-        {
-          name: 'View Employees',
-          value: 'viewEmployees'
-        },
-        {
-          name: `Update An Employee's Role`,
-          value: 'updateRole'
-        },
-        {
-          name: 'Add Department',
-          value: 'addDepartment'
-        },
-        {
-          name: 'View Departments',
-          value: 'viewDepartments'
-        },
-        {
-          name: 'Add A Role',
-          value: 'addRole'
-        },
-        {
-          name: 'View Roles',
-          value: 'viewRoles'
-        },
-      ]
-    }
-  ])
-  .then(choice => {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'option',
+        message: 'What action would you like to perform?',
+        choices: [
+          {
+            name: 'Add An Employee',
+            value: 'addEmployee'
+          },
+          {
+            name: 'View Employees',
+            value: 'viewEmployees'
+          },
+          {
+            name: `Update An Employee's Role`,
+            value: 'updateRole'
+          },
+          {
+            name: 'Add Department',
+            value: 'addDepartment'
+          },
+          {
+            name: 'View Departments',
+            value: 'viewDepartments'
+          },
+          {
+            name: 'Add A Role',
+            value: 'addRole'
+          },
+          {
+            name: 'View Roles',
+            value: 'viewRoles'
+          },
+        ]
+      }
+    ])
+    .then(choice => {
 
-    switch (choice.option) {
-      case 'addEmployee':
+      switch (choice.option) {
+        case 'addEmployee':
           addEmployee()
-        break;
-      case 'viewEmployees':
-        viewEmployees()
-        break;
-      case 'updateRole':
-        updateRole()
-        break;
-      case 'viewDepartments':
-        viewDepartments()
-        break;
-      case 'addDepartment':
-        addDepartment()
-        break;
-      case 'viewRoles':
-        viewRoles()
-        break;
-      case 'addRole':
-        addRole()
-        break;
-    }
-  })
-  .catch(error => {console.log(error)})
+          break;
+        case 'viewEmployees':
+          viewEmployees()
+          break;
+        case 'updateRole':
+          updateRole()
+          break;
+        case 'viewDepartments':
+          viewDepartments()
+          break;
+        case 'addDepartment':
+          addDepartment()
+          break;
+        case 'viewRoles':
+          viewRoles()
+          break;
+        case 'addRole':
+          addRole()
+          break;
+      }
+    })
+    .catch(error => { console.log(error) })
 }
-landingPrompt();
+start();
